6cf20a3783ca1179016918a7afd7bec2aaaa80cb
Krzysztof Ligarski <ligarski.k@gmail.com>
2026-05-20 20:38:38 +0200

    fix backend

--- backend/database.py
+++ backend/database.py
@@ -4,11 +4,14 @@ import os
 from sqlalchemy import Float, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, sessionmaker
+from sqlalchemy.pool import NullPool
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./logistics.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=NullPool,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
